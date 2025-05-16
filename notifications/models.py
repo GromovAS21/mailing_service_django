152437835279a193f1f7405018cd9d5091de546e
Anton Gromov <seed14112@yandex.ru>
2025-05-16 09:10:47 +0300

    [+] Добавлена модель Recipient

--- notifications/models.py
+++ notifications/models.py
@@ -2,6 +2,7 @@
 
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Notification(models.Model):
@@ -15,7 +16,7 @@ class Notification(models.Model):
         ONE_DAY = 2, "Задержка 1 день"
 
     message = models.TextField(
-        vernose_name="Текст уведомления",
+        verbose_name="Текст уведомления",
         validators=[
             MinLengthValidator(1, "Сообщение не может быть пустым"),
             MaxLengthValidator(1024, "Максимальная длина сообщения - 1024 символов"),
@@ -24,8 +25,42 @@ class Notification(models.Model):
     delay = models.IntegerField(
         choices=DelayChoices, default=DelayChoices.NO_DELAY, verbose_name="Задержка уведомления"
     )
+
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания уведомления")
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Возвращает номер идентификатор уведомления."""
         return f"Notification N {self.id}"
+
+
+class Recipient(models.Model):
+    """Модель получателей."""
+
+    email = (
+        models.EmailField(
+            unique=True,
+            verbose_name="Адрес получателя",
+            blank=True,
+            null=True,
+        ),
+    )
+    telegram_id = models.PositiveBigIntegerField(
+        unique=True,
+        verbose_name="Telegram ID получателя",
+        blank=True,
+        null=True,
+    )
+    notification = models.ManyToManyField(
+        Notification, related_name="recipients", verbose_name="Уведомления получателя", blank=True
+    )
+
+    def __str__(self) -> str:
+        """Возвращает email или telegram_id получателя."""
+        return self.email if self.email else f"{self.telegram_id}"
+
+    def clean(self) -> None:
+        """Проверяет, что введён либо email, либо telegram_id."""
+        if self.email and self.telegram_id:
+            raise ValidationError("Может быть введён только один адрес получателя: email или telegram_id")
+        if not self.email and not self.telegram_id:
+            raise ValidationError("Необходимо ввести адрес получателя: email или telegram_id")
