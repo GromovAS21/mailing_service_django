390e27ea4a4fa5f0a075d21317aeb46fa63e4847
Anton Gromov <seed14112@yandex.ru>
2025-05-16 20:52:50 +0300

    [+] Добавление представления

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,20 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from notifications.serializers import NotificationSerializer
 
 
 class NotifyView(APIView):
     """Представление для отправки уведомлений."""
 
-    pass
+    def post(self, request) -> Response:
+        """Отправка уведомлений."""
+        serializer = NotificationSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
